40d8a7d7d838dd1e1f597925f69f961c8241bd09
DiegoGustavoMO <71155173+DiegoGustavoMO@users.noreply.github.com>
2021-11-23 16:56:33 -0500

    modificacion en categorias iconos nombre

--- lib/helpers/iconhelper.dart
+++ lib/helpers/iconhelper.dart
@@ -2,10 +2,10 @@
 class IconFontHelper {
   static const String LOGO = "a";
   static const String MAIN_LOGO = "";
-  static const String MEATS = "c";
-  static const String DRUGSTORE = "d";
-  static const String BAKERY = "e";
-  static const String SMISCELLANY = "f";
+  static const String MEATS = "C";
+  static const String DRUGSTORE = "D";
+  static const String BAKERY = "P";
+  static const String SMISCELLANY = "M";
   static const String VEGS = "g";
   static const String FRUITS = "h";
 }
--- lib/pages/categorylistpage.dart
+++ lib/pages/categorylistpage.dart
@@ -1,5 +1,6 @@
 import 'package:apptienda/helpers/utils.dart';
 import 'package:apptienda/models/category.dart';
+import 'package:apptienda/widgets/iconfont.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -42,7 +43,36 @@ class CategoryListPage extends StatelessWidget {
                                 ".jpg",
                             fit: BoxFit.cover,
                           ),
-                        ))
+                        )),
+                        Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              height: 120,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(20),
+                                      bottomRight: Radius.circular(20)),
+                                  gradient: LinearGradient(
+                                    colors: [],
+                                    begin: Alignment.bottomCenter,
+                                    end: Alignment.topCenter,
+                                  )),
+                            )),
+                        Row(
+                          children: [
+                            Container(
+                              color: categories[index].color,
+                              padding: EdgeInsets.all(10),
+                              child: IconFont(
+                                color: Colors.white,
+                                iconName: categories[index].icon,
+                                size: 30,
+                              ),
+                            )
+                          ],
+                        )
                       ],
                     ),
                   );
